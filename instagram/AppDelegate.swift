72b517873c8e2fbabd85df8ba05c542b59b1de32
jnuel <joynuel@gmail.com>
2020-10-17 19:05:09 -0500

    finished basic instagram project

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "q37vQv83JsEYEwzp1Z70WcOvSUy5Vqzn5sDBSbvC"
+                    $0.clientKey = "i7kapbXSTzBZPZ8xTqcZVje1Lx5WTRQfyWd2bpEK"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
---
+++ instagram/postTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  postTableViewCell.swift
+//  instagram
+//
+//  Created by Joy Nuelle on 10/17/20.
+//
+
+import UIKit
+
+class postTableViewCell: UITableViewCell {
+
+    @IBOutlet var photoView: UIImageView!
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
